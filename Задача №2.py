7e5f17a33130bc626a804c39795c227a967d3387
endinggg <68166011+endinggg@users.noreply.github.com>
2022-02-06 14:25:55 +0300

    Добавил 2 задачу

---
+++ Задача №2.py
@@ -0,0 +1,74 @@
+import os
+import sys
+
+import requests
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel
+from PyQt5.QtCore import Qt
+
+SCREEN_SIZE = [600, 450]
+
+
+class Example(QWidget):
+    def __init__(self, coords, scale):
+        super().__init__()
+        self.coords = coords
+        self.scale = scale
+        self.getImage()
+        self.initUI()
+
+    def getImage(self):
+        map_request = "http://static-maps.yandex.ru/1.x/"
+        map_view = "map"
+        map_params = {"ll": ",".join(self.coords),
+                      "spn": ",".join([self.scale, self.scale]),
+                      "l": map_view}
+        response = requests.get(map_request, params=map_params)
+
+        if not response:
+            print("Ошибка выполнения запроса:")
+            print(map_request)
+            print("Http статус:", response.status_code, "(", response.reason, ")")
+            print(self.coords)
+            sys.exit(1)
+
+        self.map_file = "map.png"
+        with open(self.map_file, "wb") as file:
+            file.write(response.content)
+
+    def initUI(self):
+        self.setGeometry(100, 100, *SCREEN_SIZE)
+        self.setWindowTitle('Отображение карты')
+        self.pixmap = QPixmap(self.map_file)
+        self.image = QLabel(self)
+        self.image.move(0, 0)
+        self.image.resize(600, 450)
+        self.image.setPixmap(self.pixmap)
+
+    def update_image(self):
+        self.getImage()
+        self.pixmap = QPixmap(self.map_file)
+        self.image.setPixmap(self.pixmap)
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_PageUp:
+            if float(self.scale) < 21:
+                self.scale = str(float(self.scale) + 2)
+                print(self.scale)
+        if event.key() == Qt.Key_PageDown:
+            if float(self.scale) > 1:
+                self.scale = str(float(self.scale) - 2)
+                print(self.scale)
+        self.update_image()
+
+    def closeEvent(self, event):
+        os.remove(self.map_file)
+
+
+if __name__ == '__main__':
+    coords = input('Введите координаты объекта через пробел: ').split()
+    scale = input('Введите масштаб одним числом: ')
+    app = QApplication(sys.argv)
+    ex = Example(coords, scale)
+    ex.show()
+    sys.exit(app.exec())
